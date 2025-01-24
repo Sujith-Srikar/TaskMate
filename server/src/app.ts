627dda74f68c0f0873c49100ffa07e0db10171ca
Sujith Srikar <kandregulasujith@gmail.com>
2025-01-24 15:43:03 +0530

    new origin added

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,12 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://task-mate-s.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.use('/auth',authRoutes);
